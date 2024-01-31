bf799b7ffc30bfaed1bc0652df2fd25d4390e259
Danil <ddynikov@gmail.com>
2024-01-31 09:18:55 +0200

    inclusion sort

---
+++ 1-sort-by-inclusion/inclusionSort.js
@@ -0,0 +1,27 @@
+'use strict'
+
+// initialising start array
+const A = [30, 19, 9, 15, 55, 24, 3, 78, 46, 41];
+
+// creating 2 arrays to push arguments in
+const oddA = [];
+const evenA = [];
+
+// loop for sorting A array and find odd and even elements
+for (let value of A){
+        if (value & 1){
+            oddA.push(value);
+        } else evenA.push(value);
+}
+
+//functions that provide sorting of our arrays 
+const oddSorted = oddA.sort((a, b) => b - a);
+const evenSorted = evenA.sort((a, b) => {if (a > b) return 1;if (a == b) return 0; if (a < b) return -1;});
+
+//concating oddSorted and evenSorted
+const done = evenSorted.concat(oddSorted);
+
+//output on display
+
+console.log('starting array ' + A);
+console.dir('procedured task ' + done);
